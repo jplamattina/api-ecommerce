e023f7741c6957f3716501b5117db38593fa819e
jplamattina <jplamattina@gmail.com>
2024-07-30 10:19:57 -0300

    delete user endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const userRouter = require('./routes/users.router.js')
 const productRouter = require('./routes/products.router.js')
 const cartRouter = require('./routes/cart.router.js')
 const logger = require('morgan');
@@ -7,21 +6,16 @@ const { uploader } = require('./utils/multer.js');
 
 const app = express()
 const PORT = 8080
-//dirname() -> funcion que da la ruta padre es lo mismo __dirname
-//entities for a crud: users, products, sessions, cart, message, tickets
-//middleware(? --> Metodo, para procesar los json del clinte
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/static', express.static(__dirname + '/public')) // el dirname hace que no falle si esta en la nube
-app.use(logger('dev')) //middleware de terceros
+app.use('/static', express.static(__dirname + '/public'))
+app.use(logger('dev'))
 
 
-//endpoints
-//se pueden subir archivos en un array en vez de single
 app.post('/', uploader.single('myFile'), (req, res) => {
     res.send('file uploaded')
 })
-app.use('/api/user', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/cart', cartRouter)
 
